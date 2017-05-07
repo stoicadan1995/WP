d5e60d4a51cc7243194b7856455fdcea9b45d7ca
stoicadan1995 <sdcristian95@gmail.com>
2017-05-07 16:34:17 +0300

    Now the customized button changes text color

--- LAB_1/Lab1onPW/app/src/main/java/com/example/kp/lab1onpw/MainActivity.java
+++ LAB_1/Lab1onPW/app/src/main/java/com/example/kp/lab1onpw/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button buttonfirst;
-
+    Button buttonsecond;
+    TextView text;
 
 
 
@@ -25,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonfirst=(Button)findViewById(R.id.buttonfirst);
+        buttonsecond=(Button)findViewById(R.id.buttonsecond);
+        text=(TextView)findViewById(R.id.text);
+
+        buttonsecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setTextColor(0xff234500);
+            }
+        });
 
         buttonfirst.setOnClickListener(new View.OnClickListener() {
             @Override
