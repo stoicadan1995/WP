38c18bcfe2374712d4c661dd53e437372e8f0e84
stoicadan1995 <sdcristian95@gmail.com>
2017-05-10 20:54:22 +0300

    Added dialog box and closing button with dialog title and message.

---
+++ LAB_2/Lab2OnPW/app/src/main/java/com/example/kp/lab2onpw/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.kp.lab2onpw;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1=(Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        MainActivity.this);
+                builder.setMessage("Dialog!")
+                        .setTitle("Dialog Title")
+                        .setCancelable(false)
+                        .setPositiveButton("Close",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
+
+    }
+}
